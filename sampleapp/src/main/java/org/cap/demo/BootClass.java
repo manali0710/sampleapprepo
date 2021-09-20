16e5a452d1bc2437a52cda993288629f137217ff
manali0710 <manalichavan1999@gmail.com>
2021-09-20 11:16:36 +0530

    sum added in show method

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -9,6 +9,13 @@ public class BootClass {
 	}
 
 	public static void show() {
+		int[] arr= {1,2,3,4,4,5};
+		int sum=0;
+		for(int n:arr) {
+			sum+=n;
+			System.out.println(n);
+		}
+			
 		System.out.println("Show here!");
 	}
 }
